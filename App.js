b2e73b0d395dbbcb0d892c21e61d216473ef4328
peter_alfie <alfie1104@gmail.com>
2019-12-12 08:59:24 +0900

    #9.1 is done

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { Camera } from "expo";
+import * as Permissions from "expo-permissions";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    hasPermission: null
+  };
+
+  componentDidMount = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    if (status === "granted") {
+      this.setState({ hasPermission: true });
+    } else {
+      this.setState({ hasPermission: false });
+    }
+  };
+
+  render() {
+    const { hasPermission } = this.state;
+    if (hasPermission) {
+      return (
+        <View>
+          <Text>Has Permissions</Text>
+        </View>
+      );
+    } else if (hasPermission === false)
+      return (
+        <View>
+          <Text>Don't have Permissions for this</Text>
+        </View>
+      );
+
+    return <ActivityIndicator />;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
