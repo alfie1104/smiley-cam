a7e36425bad47c9daa15eb8ef0cfc08bfb645218
peter_alfie <alfie1104@gmail.com>
2019-12-13 08:48:43 +0900

    SmileDetection function is added

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import React from "react";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
-import { Camera } from "expo";
+import {
+  StyleSheet,
+  View,
+  ActivityIndicator,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
+import { Camera } from "expo-camera";
+import * as FaceDetector from "expo-face-detector";
 import * as Permissions from "expo-permissions";
+import styled from "styled-components";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const { width, height } = Dimensions.get("window");
+
+const CenterView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: cornflowerblue;
+`;
+
+const Text = styled.Text`
+  color: white;
+  font-size: 22px;
+`;
+
+const IconBar = styled.View`
+  margin-top: 50px;
+`;
 
 export default class App extends React.Component {
   state = {
-    hasPermission: null
+    hasPermission: null,
+    cameraType: Camera.Constants.Type.front,
+    smileDetected: false
   };
 
   componentDidMount = async () => {
@@ -18,22 +47,77 @@ export default class App extends React.Component {
   };
 
   render() {
-    const { hasPermission } = this.state;
+    const { hasPermission, cameraType, smileDetected } = this.state;
     if (hasPermission) {
       return (
-        <View>
-          <Text>Has Permissions</Text>
-        </View>
+        <CenterView>
+          <Camera
+            style={{
+              width: width - 40,
+              height: height / 1.5,
+              borderRadius: 10,
+              overflow: "hidden"
+            }}
+            type={cameraType}
+            onFacesDetected={smileDetected ? null : this.onFacesDetected}
+            faceDetectorSettings={{
+              detectLandmarks: FaceDetector.Constants.Landmarks.all,
+              runClassifications: FaceDetector.Constants.Classifications.all
+            }}
+          />
+          <IconBar>
+            <TouchableOpacity onPress={this.switchCameraType}>
+              <MaterialIcons
+                name={
+                  cameraType === Camera.Constants.Type.front
+                    ? "camera-rear"
+                    : "camera-front"
+                }
+                color="white"
+                size={50}
+              />
+            </TouchableOpacity>
+          </IconBar>
+        </CenterView>
       );
     } else if (hasPermission === false)
       return (
-        <View>
+        <CenterView>
           <Text>Don't have Permissions for this</Text>
-        </View>
+        </CenterView>
       );
 
-    return <ActivityIndicator />;
+    return (
+      <CenterView>
+        <ActivityIndicator />
+      </CenterView>
+    );
   }
+
+  switchCameraType = () => {
+    const { cameraType } = this.state;
+    if (cameraType === Camera.Constants.Type.front) {
+      this.setState({
+        cameraType: Camera.Constants.Type.back
+      });
+    } else {
+      this.setState({
+        cameraType: Camera.Constants.Type.front
+      });
+    }
+  };
+
+  onFacesDetected = ({ faces }) => {
+    const face = faces[0];
+    if (face) {
+      if (face.smilingProbability > 0.7) {
+        this.setState({
+          smileDetected: true
+        });
+        console.log("take photo");
+      }
+    }
+  };
 }
 
 const styles = StyleSheet.create({
